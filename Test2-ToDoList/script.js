b79a3d8c1f75629f2db34e9a6397869866ea204c
Akopyan Timur <56245133+sellerofair@users.noreply.github.com>
2019-10-12 11:26:45 +0300

    button Del was added

--- Test2-ToDoList/script.js
+++ Test2-ToDoList/script.js
@@ -15,11 +15,15 @@ function addItem() {
             }
         );
         document.getElementById("otputField").innerHTML = "<ul>\n"
-        + list.reduce((code, string) => code + `    <li>${string.title}</li>\n`, "")
+        + list.reduce((code, string) => code + `    <li>${string.title} <input type="button" value="Удалить" onclick="delItem()"></li>\n`, "")
         + "</ul>";
 
         document.getElementById("inputField").value = "";
     }
+}
+
+function delItem() {
+    
 }
 
 function cleanOtputField() {
