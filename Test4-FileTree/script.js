9af0cacf591193ba58ad159407f5952ebebd66d5
Akopyan Timur <56245133+sellerofair@users.noreply.github.com>
2019-11-08 02:21:03 +0300

    trying to design the tree

--- Test4-FileTree/script.js
+++ Test4-FileTree/script.js
@@ -1,8 +1,36 @@
 "use strict";
 
-let tree = {ROOT: []};
+class Folder {
+    constructor(name = "New Folder") {
+        this.name = name;
+        this.content = [];
+    }
 
-function check(str) {
-    const directString = str.toLowerCase();
-    return directString === directString.split("").reverse().join("");
+    rename(newName) {
+        this.name = newName;
+    }
+
+    addFolder(name) {
+        this.content.push(new Folder(name))
+    }
+
+    addFile(name) {
+        this.content.push(new File(name))
+    }
+
 }
+
+class File {
+    constructor(name = "Empty File.txt") {
+        this.name = name.slice(0, name.lastIndexOf("."));
+        this.extention = name.slice(name.lastIndexOf(".") + 1);
+    }
+
+    rename(newName) {
+        this.name = newName.slice(0, newName.lastIndexOf("."));
+        this.extention = newName.slice(newName.lastIndexOf(".") + 1);
+    }
+
+}
+
+let tree = new Folder("ROOT");
