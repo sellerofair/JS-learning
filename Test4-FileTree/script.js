ece54f4ea61e4016cd43e387189005ba4e3c7fea
Akopyan Timur <56245133+sellerofair@users.noreply.github.com>
2019-11-10 23:10:55 +0300

    trying to add find & filter

--- Test4-FileTree/script.js
+++ Test4-FileTree/script.js
@@ -12,6 +12,58 @@ function nameMatch(isFolder, name, folder) {
     return false;
 }
 
+function defineIndex (folder, name) {
+    
+    let index = -1;
+    
+    for (let key in folder.content) {
+        if (folder.content[key].name === name) {
+            index = key;
+        }
+    }
+
+    return index;
+}
+
+function levelMarker(level) {
+    let marker = "";
+
+    if (level > 0) {
+        for (let i = 0; i < level; i++) {
+            marker += "--"
+        }
+        marker += " ";
+    }
+
+    return marker;
+}
+
+function printTree(tree) {
+    let level = 0;
+    let printFolder = function(folder) {
+        console.log (`${levelMarker(level)}${folder.name}`);
+        level += 1;
+        for (let item of folder.content) {
+            if (item instanceof Folder) {
+                printFolder(item);
+                level -=1;
+            } else {
+                console.log (`${levelMarker(level)}${item.name}.${item.extention}`);
+            }
+        }
+    }
+    printFolder(tree);
+}
+
+function find(tree, string, matchCase) {    // matchCase === true: учитывать регистр;
+    let adress = "";
+
+}
+
+function filter(tree, extentions) {
+    let adress = "";
+}
+
 class Folder {
     constructor(name = "New Folder") {
         this.name = name;
@@ -38,6 +90,16 @@ class Folder {
         }
     }
 
+    delete(name) {
+        const index = defineIndex(this, name);
+
+        if (index === -1) {
+            alert("Объект с таким именем не найден!");
+        } else {
+            this.content.splice(index, 1);
+        }
+    }
+
 }
 
 class File {
@@ -50,9 +112,20 @@ class File {
         this.name = newName.slice(0, newName.lastIndexOf("."));
         this.extention = newName.slice(newName.lastIndexOf(".") + 1);
     }
-
-    delete()
-
 }
 
 let tree = new Folder("ROOT");
+
+tree.addFolder("FOLDER_1");
+tree.addFolder("FOLDER_2");
+tree.content[0].addFolder("FOLDER_3")
+tree.addFile("file1.txt");
+tree.addFile("file2.html");
+tree.content[0].addFile("file3.html");
+tree.content[0].addFile("file4.js");
+tree.content[0].addFile("file5.css");
+tree.content[0].content[0].addFile("file6.html");
+tree.content[0].content[0].addFile("file7.js");
+tree.content[0].content[0].addFile("file8.css");
+tree.content[1].addFile("file9.txt");
+tree.content[1].addFile("file10.txt");
