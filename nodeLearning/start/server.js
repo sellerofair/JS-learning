a76eda7709328b1b4b7de07cb3b9dbe27775f50b
Akopyan Timur <56245133+sellerofair@users.noreply.github.com>
2019-12-11 17:35:02 +0300

    separate things

--- nodeLearning/start/server.js
+++ nodeLearning/start/server.js
@@ -1,15 +1,5 @@
 "use strict"
 
-class User {
-    constructor(name) {
-        this.name = name;
-    }
-
-    hello(who) {
-        console.log(`${this.name} say Hello to ${who.name}`)
-    }
-}
-
 let bob = new User("Bob");
 let jack = new User("Jack");
 
---
+++ nodeLearning/start/user.js
@@ -0,0 +1,11 @@
+"use strict"
+
+class User {
+    constructor(name) {
+        this.name = name;
+    }
+
+    hello(who) {
+        console.log(`${this.name} say Hello to ${who.name}`)
+    }
+}
