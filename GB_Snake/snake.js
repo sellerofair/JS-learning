83b646fb7b82a79f1790f6c1dcda63734f6acb06
sellerofair <akopyan.t.i@inbox.ru>
2019-12-23 10:31:40 +0300

    Snake. Класс Point

--- GB_Snake/snake.js
+++ GB_Snake/snake.js
@@ -1,15 +1,27 @@
 "use strict"
 
-function rect(x, y, color) {
-    const canvas = window.document.getElementById("canvas").innerHTML;
-    window.document.getElementById("canvas").innerHTML = `${canvas}
-    <rect x="${x * 10}" y="${y * 10}" width="10" height="10" stroke="black" fill="${color}"/>`;
+class Point {
+    constructor(x, y, color) {
+        this.x = x,
+        this.y = y,
+        this.color = color
+    }
+
+    print() {
+        const canvas = window.document.getElementById("canvas").innerHTML;
+        window.document.getElementById("canvas").innerHTML = `${canvas}
+        <rect x="${this.x * 10}" y="${this.y * 10}" width="10" height="10" stroke="black" fill="${this.color}"/>`;
+    }
 }
 
-let x1 = 1;
-let y1 = 3;
-rect(x1, y1, "white");
+function rect(point) {
+    const canvas = window.document.getElementById("canvas").innerHTML;
+    window.document.getElementById("canvas").innerHTML = `${canvas}
+    <rect x="${point.x * 10}" y="${point.y * 10}" width="10" height="10" stroke="black" fill="${point.color}"/>`;
+}
 
-let x2 = 4;
-let y2 = 5;
-rect(x2, y2, "yellow");
+let point1 = new Point(1, 3, "white");
+point1.print();
+
+let point2 = new Point(4, 5, "yellow");
+point2.print();
