d6530dcc19921fdb6b6bfd17b34685ecb6c11a81
sellerofair <akopyan.t.i@inbox.ru>
2019-12-23 10:00:11 +0300

    Snake. Процедурный подход. Функция rect

---
+++ GB_Snake/snake.js
@@ -0,0 +1,15 @@
+"use strict"
+
+function rect(x, y, color) {
+    const canvas = window.document.getElementById("canvas").innerHTML;
+    window.document.getElementById("canvas").innerHTML = `${canvas}
+    <rect x="${x * 10}" y="${y * 10}" width="10" height="10" stroke="black" fill="${color}"/>`;
+}
+
+let x1 = 1;
+let y1 = 3;
+rect(x1, y1, "white")
+
+let x2 = 4;
+let y2 = 5;
+rect(x2, y2, "yellow")
