cd3fe3eb02c72de97f594d5aea7121b4369f811c
sellerofair <akopyan.t.i@inbox.ru>
2019-12-24 17:25:41 +0300

    ...

--- SelfTests/pizano.js
+++ SelfTests/pizano.js
@@ -4,6 +4,7 @@
 // Необходимо найти остаток от деления n-го числа Фибоначчи на m.
 
 function pizano(n, m) {
+    let beginning = new Date;
     let sequence = [0, 1];
     let i = 1;
     do {
@@ -11,10 +12,33 @@ function pizano(n, m) {
         sequence.push((sequence[i - 1] + sequence[i - 2]) % m);
     } while (sequence[i] != 1 || sequence[i - 1] != 0);
 
+    console.log(sequence);
+    console.log(n % (sequence.length - 2));
+
+    console.log(`${new Date - beginning} мс`);
+
     return sequence[n % (sequence.length - 2)];
 }
 
-let n = Math.floor(Math.random() * (10 ** 18));
-let m = Math.floor(Math.random() * (10 ** 5));
+function fib(n) {
+    if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    } else {
+        let a = 0;
+        let b = 1;
+        let c = 1;
+        for (let i = 2; i < n; i++) {
+            a = b;
+            b = c;
+            c = a + b;
+        }
+        return c;
+    }
+}
 
-console.log(`F(${n}) % F(${m}) = ${pizano(n, m)}`);
+let n = Math.floor(Math.random() * (10 ** 18)) + 1;
+let m = Math.floor(Math.random() * (10 ** 5)) + 2;
+
+console.log(`F(${n}) % ${m} = ${pizano(n, m)}`);
