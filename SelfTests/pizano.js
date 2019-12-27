261a4d13d1e7a6e369be50f54bdacf3ba1ee331f
sellerofair <akopyan.t.i@inbox.ru>
2019-12-27 12:04:38 +0300

    Pizano defeated!!!

--- SelfTests/pizano.js
+++ SelfTests/pizano.js
@@ -3,38 +3,19 @@
 // Даны целые числа 1 <= n <= 10^18 и 2 <= m <= 10^5
 // Необходимо найти остаток от деления n-го числа Фибоначчи на m.
 
-function fib(n) {
-    if (n === 0) {
-        return 0;
-    } else if (n === 1) {
-        return 1;
-    } else {
-        let a = 0;
-        let b = 1;
-        let c = 1;
-        for (let i = 2; i < n; i++) {
-            a = b;
-            b = c;
-            c = a + b;
-        }
-        return c;
-    }
-}
+let stdin = process.openStdin();
+stdin.on('data', function(data){
 
-let n = 100;
-let m = 25;
-
-let sequence = [0, 1];
-let i = 1;
-do {
-    i++;
-    sequence.push((sequence[i - 1] + sequence[i - 2]) % m);
-} while (sequence[i] != 1 || sequence[i - 1] != 0);
-
-console.log(sequence);
-console.log(`Period = ${sequence.length - 2}`);
-console.log(`Index = ${n % (sequence.length - 2)}`);
-console.log(`F(${n}) % ${m} =`);
-console.log(`${fib(n)} % ${m} =`);
-console.log(fib(n) % m);
-console.log(sequence[n % (sequence.length - 2)]);
+    let base = data.toString().split(" ");
+    let n = BigInt(base[0]);
+    let m = BigInt(base[1]);
+    
+    let sequence = [0n, 1n];
+    let i = 1;
+    do {
+        i++;
+        sequence.push((sequence[i - 1] + sequence[i - 2]) % m);
+    } while (sequence[i] !== 1n || sequence[i - 1] !== 0n);
+    
+    console.log(Number(sequence[n % BigInt((sequence.length - 2))]));
+});
