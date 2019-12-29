5c318243c97e16cf3c6c7ec19d3ee6ae18c16321
Akopyan Timur <56245133+sellerofair@users.noreply.github.com>
2019-12-29 15:18:49 +0300

    Snake. Finished

--- GB_Snake/snake.js
+++ GB_Snake/snake.js
@@ -136,6 +136,27 @@ class Snake extends Figure {
             return false;
         }
     }
+
+    isHit(figure) {
+        let head = this.list[this.list.length - 1];
+        for (let point of figure.list) {
+            if (point.isHit(head)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    isHitTale() {
+        let head = this.list[this.list.length - 1];
+        for (let i = 0; i < this.list.length - 2; i++) {
+            if (head.isHit(this.list[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
 class FoodCreator {
@@ -145,31 +166,86 @@ class FoodCreator {
         this.color = color;
     }
     
-    createFood() {
-        const x = Math.round(Math.random() * (field.width - 1)) + 1;
-        const y = Math.round(Math.random() * (field.height - 1)) + 1;
-        return new Point(x, y, this.color);
+    createFood(snake) {
+        let isSnake = true;
+        let food;
+        while(isSnake) {
+            isSnake = false;
+            let x = Math.round(Math.random() * (this.mapWidth - 1)) + 1;
+            let y = Math.round(Math.random() * (this.mapHeight - 1)) + 1;
+            food = new Point(x, y, this.color);
+            for (let point of snake.list) {
+                if (food.isHit(point)) {
+                    isSnake = true;
+                    break;
+                }
+            }
+        }
+
+        return food;
     }
 }
 
 class Walls {
-    constructor() {
-        
+    constructor(mapWidth, mapHeight) {
+        this.list = [];
+        this.mapWidth = mapWidth;
+        this.mapHeight = mapHeight;
+
+        this.list.push(new HorizontalLine(0, 0, this.mapWidth + 2));
+        this.list.push(new VerticalLine(0, 0, this.mapHeight + 2));
+        this.list.push(new HorizontalLine(0, this.mapHeight + 1, this.mapWidth + 2));
+        this.list.push(new VerticalLine(this.mapWidth + 1, 0, this.mapHeight + 2));
+    }
+
+    draw() {
+        for (let wall of this.list) {
+            wall.draw();
+        }
+    }
+
+    isHit(snake) {
+        for (let wall of this.list) {
+            if (snake.isHit(wall)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
 
 function running() {
+    if (walls.isHit(snake) || snake.isHitTale()) {
+        clearInterval(theGame);
+        alert("Game over!!!");
+        refreshField();
+    }
     if (snake.eat(food)) {
-        food = foodCreator.createFood();
+        food = foodCreator.createFood(snake);
         food.draw();
     } else {
         snake.move();
     }
 }
 
+function refreshField() {
+    document.body.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" id="canvas" width="${field.width * 10 + 20}" height="${field.height * 10 + 20}" fill="black">
+            <rect x="0" y="0" width="${field.width * 10 + 20}" height="${field.height * 10 + 20}" fill="black"/>
+        </svg>
+        <script src="./snake.js"></script>
+    `
+    walls.draw();
+    p1 = new Point(2, 2, "white");
+    snake = new Snake(p1, 4, Direcrion.RIGHT);
+    snake.draw();
+    food = foodCreator.createFood(snake);
+    food.draw();
+}
+
 const field = {
-    width: 50,
-    height: 50
+    width: 20,
+    height: 20
 }
 
 const Direcrion = {
@@ -179,30 +255,20 @@ const Direcrion = {
     DOWN: Symbol("DOWN")
 };
 
-document.body.innerHTML = `
-    <svg xmlns="http://www.w3.org/2000/svg" id="canvas" width="${field.width * 10 + 20}" height="${field.height * 10 + 20}" fill="black">
-        <rect x="0" y="0" width="${field.width * 10 + 20}" height="${field.height * 10 + 20}" fill="black"/>
-    </svg>
-    <script src="./snake.js"></script>
-`
-
 let theGame;
 
 document.addEventListener("keydown", function(event) {
     snake.handleKey(event.code);
 });
 
-(new HorizontalLine(0, 0, field.width + 2)).draw();
-(new VerticalLine(0, 0, field.width + 2)).draw();
-(new HorizontalLine(0, field.width + 1, field.width + 2)).draw();
-(new VerticalLine(field.width + 1, 0, field.width + 2)).draw();
+let walls = new Walls(field.width, field.height);
 
-let p1 = new Point(2, 2, "white");
+let p1
 
-let snake = new Snake(p1, 4, Direcrion.RIGHT);
-snake.draw();
+let snake
 
 let foodCreator = new FoodCreator (field.width, field.height, "yellow");
 
-let food = foodCreator.createFood();
-food.draw();
+let food
+
+refreshField();
