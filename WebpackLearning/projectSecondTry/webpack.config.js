913415d201823f69a693835a65122c38e0130627
Akopyan Timur <56245133+sellerofair@users.noreply.github.com>
2020-03-04 23:25:42 +0300

    Webpack. 2.4, 2.5

--- WebpackLearning/projectSecondTry/webpack.config.js
+++ WebpackLearning/projectSecondTry/webpack.config.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const NOD_ENV = process.env.NOD_ENV || "development";
+const webpack = require("webpack")
+
 module.exports = {
     entry: "./home",
     output: {
@@ -9,8 +12,16 @@ module.exports = {
 
     mode: "development",
 
-    watch: true,
+    watch: NOD_ENV == "development",
     watchOptions: {
         aggregateTimeout: 100
-    }
+    },
+
+    devtool: NOD_ENV == "development" ? "chip-inline-module-source-map" : null,
+
+    plugins: [
+        new webpack.DefinePlugin({
+            NOD_ENV: JSON.stringify(NOD_ENV)
+        })
+    ]
 }
